79a2ac39fe49a0924e027bb51d8b253a68273693
marloswn <marlos.wagner@hotmail.com>
2022-05-09 19:02:09 -0300

    Commit inicial

---
+++ iac/variables.tf
@@ -0,0 +1,12 @@
+# Variables
+variable "base_bucket_name" {
+  default = "datalake-marlos-tf"
+}
+
+variable "environment" {
+  default = "producao"
+}
+
+variable "account" {
+  default = "693916676536"
+}
