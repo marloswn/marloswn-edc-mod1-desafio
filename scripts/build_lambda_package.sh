a1e238642de8cd3663f457a0c331d38586ef5477
marloswn <marlos.wagner@hotmail.com>
2022-05-12 21:02:19 -0300

    Terraform format

--- scripts/build_lambda_package.sh
+++ scripts/build_lambda_package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-cd iac
+cd ../iac
 
 # Declara variavel para reutilização nas validações do diretório
 PACKAGE="package"
@@ -33,7 +33,7 @@ fi
 cd $PACKAGE
 
 # Declara variavel que localiza a função lambda para reutilização no código.
-LAMBDA_FUNCTION=../../etl/lambda_function.py
+LAMBDA_FUNCTION=../etl/lambda_function.py
 
 # Verifica se o arquivo lambda_function.py existe
 if [ -f $LAMBDA_FUNCTION ]
@@ -42,7 +42,7 @@ then
     echo "Copiando função Handler..."
     cp $LAMBDA_FUNCTION .
     echo "Compactando arquivo lambda_function_payload.zip"
-    zip -r9 ../iac/lambda_function_payload.zip . #Compacta o pacote para o deploy
+    zip -r9 ../lambda_function_payload.zip . #Compacta o pacote para o deploy
     echo "Arquivo compactado com sucesso!"
     echo "============================================="
 fi
